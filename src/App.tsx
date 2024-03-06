37b0f0dc76588763fd1464dd7fe20529cee21ada
Masud <masudrahman.dev@gmail.com>
2024-03-06 14:41:41 +0600

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>test fonts</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque dolorem quasi, dolore id, ut reiciendis eius dicta sunt doloremque ex atque voluptate. Voluptatum recusandae voluptates, magni quibusdam minima vel harum!</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: "#101010",
+      blue: {
+        light: "#D2DCE8",
+        DEFAULT: " #32A7E2",
+      },
+      gray: "#404852",
+      green: "#31BA96",
+      red: "#DC3434",
+      purple: "#B548C6",
+      yellow: "#FF8700",
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
